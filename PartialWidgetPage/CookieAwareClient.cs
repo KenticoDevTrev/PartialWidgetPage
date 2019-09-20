ed7765d5952cfb0e607ca4909373b5ca1864af4f
Trevor Fayas <tfayas@hbs.net>
2019-09-20 16:05:13 -0500

    Added CookieAwareClient to fix issue that Server side requests were missing authentication cookies so they were not authenticating

---
+++ PartialWidgetPage/CookieAwareClient.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using static System.Collections.Specialized.NameObjectCollectionBase;
+
+namespace PartialWidgetPage
+{
+    public class CookieAwareWebClient : WebClient
+    {
+        public CookieContainer CookieContainer { get; set; }
+        public Uri Uri { get; set; }
+
+        public CookieAwareWebClient()
+            : this(new CookieContainer())
+        {
+        }
+
+        /// <summary>
+        /// Creates web client with cookies
+        /// </summary>
+        /// <param name="cookies"></param>
+        public CookieAwareWebClient(CookieContainer cookies)
+        {
+            CookieContainer = cookies;
+        }
+
+        /// <summary>
+        /// Creates web client with the cookies of the given Request
+        /// </summary>
+        /// <param name="CurrentRequest">The current request to copy cookie from</param>
+        public CookieAwareWebClient(HttpRequest CurrentRequest)
+        {
+            // Copy Cookies
+            CookieContainer cookieJar = new CookieContainer();
+            foreach (string CookieKey in CurrentRequest.Cookies)
+            {
+                var Cookie = CurrentRequest.Cookies[CookieKey];
+                // CookieContainer requires a domain. so if it's empty, then use the Current Request's Host
+                cookieJar.Add(new Cookie(CookieKey, Cookie.Value, Cookie.Path, !string.IsNullOrWhiteSpace(Cookie.Domain) ? Cookie.Domain : CurrentRequest.Url.Host));
+            }
+            CookieContainer = cookieJar;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            WebRequest request = base.GetWebRequest(address);
+            if (request is HttpWebRequest)
+            {
+                (request as HttpWebRequest).CookieContainer = this.CookieContainer;
+            }
+            HttpWebRequest httpRequest = (HttpWebRequest)request;
+            httpRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            return httpRequest;
+        }
+
+        protected override WebResponse GetWebResponse(WebRequest request)
+        {
+            WebResponse response = base.GetWebResponse(request);
+            String setCookieHeader = response.Headers[HttpResponseHeader.SetCookie];
+
+            //do something if needed to parse out the cookie.
+            if (setCookieHeader != null)
+            {
+                Cookie cookie = new Cookie(); //create cookie
+                this.CookieContainer.Add(cookie);
+            }
+
+            return response;
+        }
+    }
+}
--- PartialWidgetPage/PartialWidgetPageExtensions.cs
+++ PartialWidgetPage/PartialWidgetPageExtensions.cs
@@ -5,6 +5,7 @@ using CMS.Localization;
 using CMS.SiteProvider;
 using Kentico.PageBuilder.Web.Mvc;
 using Kentico.Web.Mvc;
+using PartialWidgetPage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,7 +32,7 @@ public static class PartialWidgetPageExtensions
     /// <returns>The rendered content</returns>
     public static HtmlString PartialWidgetPage(this HtmlHelper helper, string Path, string RenderAsPartialUrlParameter = null, bool PathIsNodeAliasPath = false)
     {
-        using (WebClient client = new WebClient())
+        using (CookieAwareWebClient client = new CookieAwareWebClient(HttpContext.Current.Request))
         {
             string url = GetRequestUrl(Path, RenderAsPartialUrlParameter, PathIsNodeAliasPath);
             try
