98ecf560c4594d2429d60eebd4561a1dfbb5c3d1
Trevor Fayas <tfayas@hbs.net>
2022-09-21 10:55:02 -0500

    Remove KX12 / KX13 full framework

--- K12/PartialWidgetPage/PartialWidgetPageWidget.cs
+++
@@ -1,4 +0,0 @@
-﻿using Kentico.PageBuilder.Web.Mvc;
-using PartialWidgetPage;
-
-[assembly: RegisterWidget("PartialWidgetPage.PartialWidget", "Partial Widget Page", typeof(PartialWidgetPageModel), customViewName: "Widgets/PartialWidgetPage/_PartialWidgetPage", Description = "Render a page's content with it's widgets, layout should use Html.LayoutIfEditMode() so will render as a partial view if pulling in.", IconClass = "icon-doc-torn")]
